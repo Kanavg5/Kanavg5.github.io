ced2390dd70cba9282b2926d058998d7999627c8
Kanavg5 <kanavthechamp@hotmail.com>
2022-10-09 18:03:36 -0400

    e

--- transitions.js
+++ transitions.js
@@ -1,3 +1,4 @@
+/*
 function waitBeforeNavigate(ev) {
     ev.preventDefualt();
     const goTo = this.getAttribute("href");
@@ -13,9 +14,8 @@ document.querySelectorAll("a")
 .forEach(EL => EL.addEventListener ("click", waitBeforeNavigate));
 
 function transition() {
-    /*
     document.getElementById('blue').style.animation = "righttransition 1s ease forwards";
     document.getElementById('yellow').style.animation = "righttransition 1.5s ease forwards";
     document.getElementById('green').style.animation = "righttransition 2s ease forwards";
-    */
 }
+*/
